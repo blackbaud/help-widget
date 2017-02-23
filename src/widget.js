dfc5c9f1a194c4e985f5ccc5e41c1c05a5c2465a
Blackbaud Sky Build User <sky-build-user@blackbaud.com>
2017-02-23 14:41:46 -0500

    Added stylesheet to page

--- src/widget.js
+++ src/widget.js
@@ -2,11 +2,16 @@
 function createWidget() {
   const body = document.getElementsByTagName('body')[0];
   const iframe = document.createElement('iframe');
+  const link = document.createElement('link');
 
   iframe.src = 'https://sky.blackbaud-dev.com/help/renxt/getting-started';
   iframe.className = 'bb-help-iframe';
 
+  link.rel = 'stylesheet';
+  link.href = 'bb-help-widget.css';
+
   body.appendChild(iframe);
+  document.head.appendChild(link);
 
   return Object.freeze({});
 }
